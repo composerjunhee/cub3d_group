215ddddf90b2270b2e49803d81128dac4fb8a266
Rasmus Rask <rasse.rask@gmail.com>
2023-11-16 12:37:19 +0200

    Enhance map validation to handle non-legal lines

--- src/cub3d.h
+++ src/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: rrask <rrask@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 10:54:41 by rrask             #+#    #+#             */
-/*   Updated: 2023/11/15 13:11:38 by rrask            ###   ########.fr       */
+/*   Updated: 2023/11/16 12:24:55 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 # define CUB3D_H
 # define MAP_PATH "../map/basic.cub"
 # define TEXTURE_PATH "../textures/"
+# define SCREEN_WIDTH 960
+# define SCREEN_HEIGHT 540
 
+# include "libft.h"
 # include <MLX42/MLX42.h>
 # include <fcntl.h>
-# include "libft.h"
 
-typedef struct s_params {
-	int		x_pos;
-	int		y_pos;
-	char	**map;
-	char	*no_texture;
-	char	*so_texture;
-	char	*we_texture;
-	char	*ea_texture;
-
-	char	*f_values;
-	char	*c_values;
-}				t_params;
+typedef struct s_params
+{
+	int				x_pos;
+	int				y_pos;
+	char			**map;
+	mlx_texture_t	*no_texture;
+	mlx_texture_t	*ea_texture;
+	mlx_texture_t	*we_texture;
+	mlx_texture_t	*so_texture;
+	char			*f_values;
+	char			*c_values;
+}					t_params;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,20 +6,57 @@
 /*   By: rrask <rrask@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 13:24:57 by rrask             #+#    #+#             */
-/*   Updated: 2023/11/15 16:30:52 by rrask            ###   ########.fr       */
+/*   Updated: 2023/11/16 12:35:59 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+// Needs to remove the prefix before each texture string before attempting to load it.
+static void	handle_params(char *line, t_params *params)
+{
+	if (ft_strncmp("NO ", (const char *)line, 3) == 0)
+		params->no_texture = mlx_load_png(line);
+	else if (ft_strncmp("SO ", (const char *)line, 3) == 0)
+		params->so_texture = mlx_load_png(line);
+	else if (ft_strncmp("WE ", (const char *)line, 3) == 0)
+		params->we_texture = mlx_load_png(line);
+	else if (ft_strncmp("EA ", (const char *)line, 3) == 0)
+		params->ea_texture = mlx_load_png(line);
+	else if (ft_strncmp("F ", (const char *)line, 2) == 0)
+		params->f_values = ft_strdup(line);
+	else if (ft_strncmp("C ", (const char *)line, 2) == 0)
+		params->c_values = ft_strdup(line);
+}
+
+static int	handle_map(char *line, t_params *params)
+{
+	int		count;
+	int		idx;
+
+	idx = 0;
+	count = 0;
+		// This does not handle lines that aren't map legal.
+	while (line[idx])
+	{
+		if (line[idx] != ' ' && line [idx] != '\t' \
+			&& line [idx] != '\n' && line [idx] != '\0')
+		{
+			count++;
+			idx = 0;
+			break ;
+		}
+		idx++;
+	}
+	return (count);
+}
+
 static int	is_file_valid(int fd, t_params *params)
 {
 	char	*line;
 	int		count;
-	int		idx;
 
 	count = 0;
-	idx = 0;
 	line = get_next_line(fd);
 	if (!line)
 		return (-1);
@@ -27,32 +64,8 @@ static int	is_file_valid(int fd, t_params *params)
 	{
 		if (!line)
 			break ;
-		if (ft_strncmp("NO ", (const char *)line, 3) == 0)
-			params->no_texture = ft_strdup(line);
-		else if (ft_strncmp("SO ", (const char *)line, 3) == 0)
-			params->so_texture = ft_strdup(line);
-		else if (ft_strncmp("WE ", (const char *)line, 3) == 0)
-			params->we_texture = ft_strdup(line);
-		else if (ft_strncmp("EA ", (const char *)line, 3) == 0)
-			params->ea_texture = ft_strdup(line);
-		else if (ft_strncmp("F ", (const char *)line, 2) == 0)
-			params->f_values = ft_strdup(line);
-		else if (ft_strncmp("C ", (const char *)line, 2) == 0)
-			params->c_values = ft_strdup(line);
-		else
-		{
-			while (line[idx])
-			{
-				if (line[idx] != ' ' && line [idx] != '\t' && line [idx] != '\n' && line [idx] != '\0')
-				{
-					ft_printf("%s", line);
-					count++;
-					idx = 0;
-					break ;
-				}
-				idx++;
-			}
-		}
+		handle_params(line, params);
+		count = handle_map(line, params);
 		free(line);
 		line = NULL;
 		line = get_next_line(fd);
@@ -68,18 +81,20 @@ static void	get_map_params(int fd, t_params *params)
 	ft_printf("lines are: %d\n", file_is_valid);
 	if (file_is_valid)
 	{
-
+		
 	}
 }
 
-static void	init_params(t_params *params)
+static void	init_params(t_params *params, mlx_t *mlx)
 {
+	mlx = mlx_init(SCREEN_WIDTH, SCREEN_HEIGHT, "Vi dy a gem", true);
 	ft_bzero(params, sizeof(*params));
 }
 
 int	main(int argc, char **argv)
 {
 	t_params	params;
+	mlx_t		*mlx;
 	int			fd;
 
 	if (argc != 2)
@@ -87,7 +102,7 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Need a path to the .cub file.", 2);
 		return (-1);
 	}
-	init_params(&params);
+	init_params(&params, &mlx);
 	fd = open(argv[1], 2);
 	if (fd == -1)
 	{
