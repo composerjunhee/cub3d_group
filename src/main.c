bffe185a13ca3563cb24b682e2c13f91a197d737
Rasmus Rask <rasse.rask@gmail.com>
2023-11-17 15:46:15 +0200

    Code refactored a little bit

--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 /*   By: rrask <rrask@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 13:24:57 by rrask             #+#    #+#             */
-/*   Updated: 2023/11/17 15:10:05 by rrask            ###   ########.fr       */
+/*   Updated: 2023/11/17 15:45:27 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
 
 static void	free_map_params(t_params *params)
 {
@@ -38,58 +37,39 @@ static void	free_map_params(t_params *params)
 	// }
 }
 
+static	void	load_texture(char *line, mlx_texture_t **texture)
+{
+	int		len;
+
+	len = ft_strlen(line);
+	line[len - 1] = '\0';
+	*texture = mlx_load_png(line);
+}
+
 // Needs to remove the prefix before each texture string before attempting to load it.
 static int	handle_params(char *line, t_params *params)
 {
-	char	*trimmed;
-	char	*trimmed_more;
-	int		param_count;
+	int	param_count;
 
-	trimmed = NULL;
-	trimmed_more = NULL;
 	param_count = 0;
 	if (ft_strncmp("NO ", (const char *)line, 3) == 0)
 	{
-		trimmed = ft_strtrim(line, "NO ");
-		trimmed_more = ft_strtrim(trimmed, "\n");
-		params->no_texture = mlx_load_png(trimmed_more);
-		free(trimmed);
-		free(trimmed_more);
-		trimmed = NULL;
-		trimmed_more = NULL;
+		load_texture(line + 3, &(params->no_texture));
 		param_count++;
 	}
 	else if (ft_strncmp("SO ", (const char *)line, 3) == 0)
 	{
-		trimmed = ft_strtrim(line, "SO ");
-		trimmed_more = ft_strtrim(trimmed, "\n");
-		params->so_texture = mlx_load_png(trimmed_more);
-		free(trimmed);
-		free(trimmed_more);
-		trimmed = NULL;
-		trimmed_more = NULL;
+		load_texture(line + 3, &(params->so_texture));
 		param_count++;
 	}
 	else if (ft_strncmp("WE ", (const char *)line, 3) == 0)
 	{
-		trimmed = ft_strtrim(line, "WE ");
-		trimmed_more = ft_strtrim(trimmed, "\n");
-		params->we_texture = mlx_load_png(trimmed_more);
-		free(trimmed);
-		free(trimmed_more);
-		trimmed = NULL;
-		trimmed_more = NULL;
+		load_texture(line + 3, &(params->we_texture));
 		param_count++;
 	}
 	else if (ft_strncmp("EA ", (const char *)line, 3) == 0)
 	{
-		trimmed = ft_strtrim(line, "EA ");
-		trimmed_more = ft_strtrim(trimmed, "\n");
-		params->ea_texture = mlx_load_png(trimmed_more);
-		free(trimmed);
-		free(trimmed_more);
-		trimmed = NULL;
-		trimmed_more = NULL;
+		load_texture(line + 3, &(params->ea_texture));
 		param_count++;
 	}
 	else if (ft_strncmp("F ", (const char *)line, 2) == 0)
@@ -107,16 +87,16 @@ static int	handle_params(char *line, t_params *params)
 
 static int	handle_map(char *line)
 {
-	int		count;
-	int		idx;
+	int	count;
+	int	idx;
 
 	idx = 0;
 	count = 0;
-		// This does not handle lines that aren't map legal.
+	// This does not handle lines that aren't map legal.
 	while (line[idx])
 	{
-		if (line[idx] != ' ' && line [idx] != '\t' \
-			&& line [idx] != '\n' && line [idx] != '\0')
+		if (line[idx] != ' ' && line[idx] != '\t' && line[idx] != '\n'
+			&& line[idx] != '\0')
 		{
 			count++;
 			idx = 0;
