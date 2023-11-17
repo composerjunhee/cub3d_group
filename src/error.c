072c1ad715e95aadde3752e96fc38f89e2765b53
Junhee Kim <junheeki@c2r5p3.hive.fi>
2023-11-17 15:40:14 +0200

    error

---
+++ src/error.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junheeki <junheeki@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 15:21:39 by junheeki          #+#    #+#             */
+/*   Updated: 2023/11/17 15:39:23 by junheeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	error_handler(int error_code)
+{
+	if (error_code == 1)
+		ft_putstr_fd("Error 1\n", 2);
+	else if (error_code == 2)
+		ft_putstr_fd("Error 2\n", 2);
+	else
+		ft_putstr_fd("Unknown error occurred\n", 2);
+}
